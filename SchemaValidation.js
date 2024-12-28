eb808ffc337235ece742e86412de93e51facae6e
shubham <shubhamsagar628@gmail.com>
2024-12-28 10:57:34 +0530

    Delete Multiple Review Middleware

--- SchemaValidation.js
+++ SchemaValidation.js
@@ -5,7 +5,7 @@ const listingSchema  = joi.object({
         title:joi.string().required(),
         description:joi.string().required(),
         image:joi.allow("",null),
-        price:joi.number().required,
+        price:joi.number().required(),
         location:joi.string().required(),
         country:joi.string().required()
 
--- models/Listing.js
+++ models/Listing.js
@@ -1,4 +1,5 @@
 const mongoose  = require('mongoose');
+const Review = require('./Review.js')
 const Schema = mongoose.Schema;
 
 const listingSchema = new Schema({
@@ -33,6 +34,14 @@ const listingSchema = new Schema({
         }
     ]
 })
+
+
+listingSchema.post("findOneAndDelete",async(listing)=>{
+    if(listing){
+        await Review.deleteMany({_id:{$in:listing.review}})
+    }
+})
+
 
 const Listing = mongoose.model("Listing",listingSchema)
 
